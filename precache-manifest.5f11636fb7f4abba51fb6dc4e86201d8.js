f79af294b19732ad68b1261694c325de0b4f0472
Ronald Gomez <rgomez2978@gmail.com>
2022-06-12 00:40:38 -0400

    Updates

---
+++ precache-manifest.5f11636fb7f4abba51fb6dc4e86201d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46a9dbeb93f6e02fcdfcde8a0d384af",
+    "url": "/react-cotizador-criptomonedas/index.html"
+  },
+  {
+    "revision": "3daafdcb5d44843e9f96",
+    "url": "/react-cotizador-criptomonedas/static/css/main.99db8a19.chunk.css"
+  },
+  {
+    "revision": "88839fb5aac0a123127b",
+    "url": "/react-cotizador-criptomonedas/static/js/2.146fad28.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/react-cotizador-criptomonedas/static/js/2.146fad28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3daafdcb5d44843e9f96",
+    "url": "/react-cotizador-criptomonedas/static/js/main.d34a0014.chunk.js"
+  },
+  {
+    "revision": "c31c2dad69c998f3c5fa",
+    "url": "/react-cotizador-criptomonedas/static/js/runtime-main.a9b7b2e3.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/react-cotizador-criptomonedas/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
